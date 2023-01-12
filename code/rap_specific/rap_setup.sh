c60f5bc58cd9d12189e831bc78a8d54162dabe92
craut <chinmay0raut@gmail.com>
2023-01-12 15:54:09 -0500

    completed v1 of rap_setup

---
+++ code/rap_specific/rap_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# this script setups up the env on the rap
+
+# give the conda installations in a neat copy paste format
+echo -e "Run following conda install cmds:\n"
+echo -e "conda install -c bioconda -y bcftools\nconda install -c conda-forge -y numpy\nconda install -c conda-forge -y htop\nconda install -c conda-forge -y parallel\n"
+
+# link the input to the home dir:
+cd ~/
+ln -s /mnt/project/Bulk/Exome\ sequences/Population\ level\ exome\ OQFE\ variants\,\ pVCF\ format\ -\ final\ release/ pVCF
+
+# create the output dirs
+mkdir -p output
+for ((i=1;i<=10;i++))
+do
+  cd ~/output
+  mkdir -p group${i}
+done
