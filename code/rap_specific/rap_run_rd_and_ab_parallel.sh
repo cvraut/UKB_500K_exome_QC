cd95319449aaa4071fcd4bba91a1b39a9303d37e
craut <chinmay0raut@gmail.com>
2023-01-12 16:34:36 -0500

    changed reference directory

--- code/rap_specific/rap_run_rd_and_ab_parallel.sh
+++ code/rap_specific/rap_run_rd_and_ab_parallel.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # run the read_depth & allelic balance filters in parallel on rap (should be run from home dir)
 
-parallel --link . /opt/notebooks/UKB_500K_exome_QC/code/read_depth_and_allele_balance/batch_process.sh --file={1} --out={2} :::: /opt/notebooks/UKB_500K_exome_QC/code/rap_specific/vcf.list /opt/notebooks/UKB_500K_exome_QC/code/rap_specific/out.list
+cd /opt/notebooks/UKB_500K_exome_QC/code/read_depth_and_allele_balance/
+
+parallel --link ./batch_process.sh --file={1} --out={2} :::: /opt/notebooks/UKB_500K_exome_QC/code/rap_specific/vcf.list /opt/notebooks/UKB_500K_exome_QC/code/rap_specific/out.list
