56efe7d0ed53c785972e1451cbc3a9450d6f0eba
craut <chinmay0raut@gmail.com>
2023-01-17 17:33:35 -0500

    fixed path issue

--- code/filter_bgen/filter_bgen.sh
+++ code/filter_bgen/filter_bgen.sh
@@ -8,8 +8,8 @@ for ((i=1;i<=22;i++))
 do
 cd ${DATA_OUT}final_qc_plink/chr${i}
 
-plink2 --bgen ${DATA_IN}ukb23159_c${i}_b0_v1.bgen 'ref-first' \
-       --sample ${DATA_IN}ukb23159_c${i}_b0_v1.sample \
+plink2 --bgen ${DATA_IN}BGEN/ukb23159_c${i}_b0_v1.bgen 'ref-first' \
+       --sample ${DATA_IN}BGEN/ukb23159_c${i}_b0_v1.sample \
        --extract final_qced_variants.list \
        --make-bed \
        --out ukbb_500k_exome_final_qced_hq_chr${i}
